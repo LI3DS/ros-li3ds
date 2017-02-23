21c45ae4ae72e1b63b172fbea7be0827264f9ac7
ATTY Lionel <lionel.atty@ign.fr>
2017-02-23 17:00:31 +0100

    [arduino] formattage de 'setenv.sh'

--- arduino/setenv.sh
+++ arduino/setenv.sh
@@ -7,10 +7,9 @@ DOCKER_BUILD_TAG=li3ds/arduino:latest
 #
 DOCKER_RUN_OPTIONS+="-it --rm"
 #
-DOCKER_RUN_OPTIONS+="					\
---volume-driver=local 					\
--v li3ds_catkin_ws:/catkin_ws 			\
--v $(realpath LI3DS_ARDUINO/):/root/."
+DOCKER_RUN_OPTIONS+=" --volume-driver=local "
+DOCKER_RUN_OPTIONS+=" -v li3ds_catkin_ws:/catkin_ws "
+DOCKER_RUN_OPTIONS+=" -v $(realpath LI3DS_ARDUINO/):/root/. "
 # share USB/serial arduino device
 # url: https://reprage.com/post/how-to-deploy-docker-containers-to-an-arduino
 DOCKER_RUN_OPTIONS+=" --privileged -v /dev/bus/usb:/dev/bus/usb"
