75cbe0d924e44aa68608c77aec4303e10278c953
ATTY Lionel <lionel.atty@ign.fr>
2017-02-23 11:20:24 +0100

    Reformat scripts/create_volume.sh

--- scripts/create_volume.sh
+++ scripts/create_volume.sh
@@ -2,6 +2,8 @@
 
 source ./setenv.sh
 
-docker volume create -d $DOCKER_VOLUME_DRIVER $DOCKER_VOLUME_NAME 
+docker volume create 					\
+	--driver 	$DOCKER_VOLUME_DRIVER 	\
+	--name 		$DOCKER_VOLUME_NAME 
 
 docker volume inspect $DOCKER_VOLUME_NAME 
